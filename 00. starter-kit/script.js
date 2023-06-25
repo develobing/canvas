6d169a8dbf55be6d58753734318144dfddd9d80a
Robin <philoin88@gmail.com>
2023-06-25 11:39:02 +0900

    폴더 구조 변경

--- 00. starter-kit/script.js
+++
@@ -1,4 +0,0 @@
-onload = () => {
-  var canvas = document.getElementById('canvas');
-  var context = canvas.getContext('2d');
-};
--- projects/02. flappy-monster/js/script.js
+++
@@ -1,8 +0,0 @@
-onload = () => {
-  // Canvas
-  var canvas = document.getElementById('flappy-monster-game');
-
-  // Game Object
-  var flappyMonster = new FlappyMonster(canvas);
-  flappyMonster.start();
-};
